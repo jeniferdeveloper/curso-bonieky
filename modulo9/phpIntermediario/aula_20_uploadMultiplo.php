6595df73ee2cd38279be037bfe46cb2502f729a0
Jenifer Maria <msn.da.jenny@gmail.com>
2018-05-21 18:36:05 -0300

    Upload de multiplos arquivos

---
+++ modulo9/phpIntermediario/aula_20_uploadMultiplo.php
@@ -0,0 +1,54 @@
+<pre>
+<?php
+
+//mostra na tela se esta funcionando o envio
+//print_r($_FILES);
+
+// verificar se houve envio
+if(isset($_FILES['arquivo'])) {
+    // //Envio de 1 único arquivo
+    // $nome = $_FILES['arquivo']['name']; //arquivo.jpg
+
+    // //Envio de multiplos arquivos
+    // $nome = $_FILES['arquivo']['name']; //array
+
+    if(count($_FILES['arquivo']['tmp_name']) > 0) {
+
+        for ($q = 0; $q < count($_FILES['arquivo']['tmp_name']); $q++) {
+
+            $nomeArquivo = md5($_FILES['arquivo']['tmp_name'][$q].time());
+
+            move_uploaded_file($_FILES['arquivo']['tmp_name'][$q],'/var/www/html/Boni/modulo9/phpIntermediario/arquivos/'.$nomeArquivo);
+        }
+    }
+
+
+}
+
+
+?>
+</pre>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload de Arquivos</title>
+</head>
+<body>
+    <!-- enctype = tipo de encode/ criptação de um arquivo -->  
+    <form method="post" enctype="multipart/form-data" action="">
+     
+     Arquivo: <br>
+     <!-- multiple deixa selecionar vários arquivos -->
+     <!-- arquivo[] se torna um array e pode receber multiplos registros -->
+     <input type="file" name="arquivo[]" multiple> <br> <br>
+
+     <input type="submit" value="Enviar">
+    
+    </form>
+    
+</body>
+</html>
